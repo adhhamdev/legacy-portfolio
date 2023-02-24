4985b7ee0568a47c4005a09b8659a833d7744213
msmadhhamg@gmail.com <msmadhhamg@gmail.com>
2023-02-24 15:03:14 +0530

    add menu

--- script.js
+++ script.js
@@ -7,4 +7,22 @@ window.addEventListener('DOMContentLoaded', () => {
     const observer = new IntersectionObserver(handleObserve, {'threshold': .5});
     const faders = document.querySelectorAll('.fader');
     faders.forEach(fader => observer.observe(fader));
-})
+});
+
+const [menuBtn, menubar] = document.querySelectorAll('.menuBtnContainer button, .menubar');
+
+let menuActive;
+menuBtn.addEventListener('click', () => {
+  if(menubar.style.opacity == '1'){
+  menubar.style = 'opacity: 0; top: -400px';
+
+}else{
+  menubar.style = 'opacity: 1; top: 55px;';
+}
+});
+
+window.addEventListener('scroll', () => {
+  if (menubar.style.opacity == '1') {
+    menubar.style = 'opacity: 0; top: -400px';
+  }
+});
