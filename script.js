376edd559e4182f7076966ad237da4be407ed498
msmadhhamg@gmail.com <msmadhhamg@gmail.com>
2023-02-25 10:16:48 +0530

    update menubar design

--- script.js
+++ script.js
@@ -7,24 +7,24 @@ window.addEventListener('DOMContentLoaded', () => {
     const observer = new IntersectionObserver(handleObserve, {'threshold': .5});
     const faders = document.querySelectorAll('.fader');
     faders.forEach(fader => observer.observe(fader));
-});
-
-const [menuBtn, menubar] = document.querySelectorAll('.menuBtnContainer button, .menubar');
-
-let menuActive;
-menuBtn.addEventListener('click', () => {
-  if(menubar.style.opacity == '1'){
-  menubar.style = 'opacity: 0; top: -400px';
-  menuBtn.querySelector('i').className = 'fas fa-bars';
-
-}else{
-  menubar.style = 'opacity: 1; top: 55px;';
-  menuBtn.querySelector('i').className = 'fas fa-arrow-up';
-}
-});
-
-window.addEventListener('scroll', () => {
-  if (menubar.style.opacity == '1') {
-    menubar.style = 'opacity: 0; top: -400px';
-  }
+    const [menuBtn, menubar] = document.querySelectorAll('.menuBtnContainer button, .menubar');
+    
+    let menuActive;
+    menuBtn.addEventListener('click', () => {
+      if (menubar.style.opacity == '1') {
+        menubar.style = 'opacity: 0; top: -400px';
+        menuBtn.querySelector('i').className = 'fas fa-bars';
+    
+      } else {
+        menubar.style = 'opacity: 1; top: 55px;';
+        menuBtn.querySelector('i').className = 'fas fa-arrow-up';
+      }
+    });
+    
+    window.addEventListener('scroll', () => {
+      if (menubar.style.opacity == '1') {
+        menubar.style = 'opacity: 0; top: -400px';
+        menuBtn.querySelector('i').className = 'fas fa-bars';
+      }
+    });
 });
